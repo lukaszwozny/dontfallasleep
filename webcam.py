7d82394b3c8efb4c607105be3ffc8165aca4e852
cp24-Komputer\cp24 <kontakt@lukaszwozny.com>
2017-11-14 10:43:46 +0100

    detect only eyes

--- webcam.py
+++ webcam.py
@@ -17,7 +17,7 @@ def get_detected_img(img, gray_img):
     img_copy = img.copy()
     faces = detect_face(gray_img=gray_img)
 
-    all_eyes = []
+    all_eyes_with_hight = []
     for (x, y, w, h) in faces:
         cv2.rectangle(img_copy, (x, y), (x + w, y + h), (255, 0, 0), 2)
         roi_gray = gray_img[y:y + h, x:x + w]
@@ -30,20 +30,34 @@ def get_detected_img(img, gray_img):
             ab_ex = x + ex
             eye_img = img[ab_ey:ab_ey + eh, ab_ex:ab_ex + ew]
             # show_all_detected_eyes(eye_img, i, img_all_eyes)
-            all_eyes.append(eye_img)
+            all_eyes_with_hight.append((eye_img, ey))
             cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
             i += 1
 
     # if img_all_eyes is not None:
     #     cv2.imshow('eyes', img_all_eyes)
     # print(len(all_eyes))
-    show_all_detected_eyes(all_eyes)
+    show_all_detected_eyes(all_eyes_with_hight)
     return img_copy
 
 
 def show_all_detected_eyes(eyes):
     i = 0
+    max_value = 0
+    max_value_2 = 0
     for eye in eyes:
-        cv2.imshow('test' + str(i), eye)
-        cv2.imwrite('eyes//eye_' + str(i) + '.jpg', eye)
+        y = eye[1]
+        if y > max_value:
+            max_value_2 = max_value
+            max_value = y
+        elif y > max_value_2:
+            max_value_2 = y
+
+    display_iter = 0
+    for eye in eyes:
+        y = eye[1]
+        if y == max_value or y == max_value_2:
+            cv2.imshow('test' + str(display_iter), eye[0])
+            display_iter += 1
+            # cv2.imwrite('eyes//eye_' + str(i) + '.jpg', eye)
         i += 1
